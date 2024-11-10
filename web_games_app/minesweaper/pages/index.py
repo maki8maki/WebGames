e4639846aabc297aafdcc737c21e8f194ac2e08d
maki8maki <maki8maki.tech@gmail.com>
2024-11-10 22:44:53 +0900

    難易度遷移画面の体裁を整えた

--- web_games_app/minesweaper/pages/index.py
+++ web_games_app/minesweaper/pages/index.py
@@ -6,18 +6,33 @@ from ...style import BOX_STYLE
 from ...templates.minesweaper import ms_pages
 from .minesweaper import MineSweaperState
 
+CUSTOM_BOX_STYLE = BOX_STYLE.copy()
+CUSTOM_BOX_STYLE.update({"padding": "0em"})
+
 
 def difficulty_component(text: str, height: int, width: int, num_mines: int):
-    txt = rx.text(f"{text} {height}x{width} {num_mines}")
     return rx.box(
-        txt,
+        rx.vstack(
+            rx.text(text, weight="bold"),
+            rx.text(f"{height} x {width}"),
+            rx.text(f"{num_mines} mines"),
+            align="center",
+            spacing="0",
+        ),
         on_click=[MineSweaperState.set_state(height, width, num_mines), rx.redirect("/minesweaper/play")],
-        style=BOX_STYLE,
+        style=CUSTOM_BOX_STYLE,
+        width="200px",
     )
 
 
 def custom_component():
-    return rx.box("Custom", on_click=rx.redirect("/minesweaper/custom"), style=BOX_STYLE)
+    return rx.box(
+        rx.text("\nCustom\n\n", weight="bold", white_space="pre"),
+        on_click=rx.redirect("/minesweaper/custom"),
+        style=CUSTOM_BOX_STYLE,
+        width="200px",
+        text_align="center",
+    )
 
 
 @rx.page(route="/minesweaper", title="Mine Sweaper")
